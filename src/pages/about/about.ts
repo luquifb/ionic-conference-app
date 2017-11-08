8edcf2a0b669b6ff3bfcc6b0108826d899dd1c11
Lucas <lfbodereau@gmail.com>
2017-11-07 21:07:36 -0300

    data change

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { PopoverController } from 'ionic-angular';
-
-import { PopoverPage } from '../about-popover/about-popover';
 
 @Component({
   selector: 'page-about',
@@ -10,10 +7,7 @@ import { PopoverPage } from '../about-popover/about-popover';
 export class AboutPage {
   conferenceDate = '2018-05-12';
 
-  constructor(public popoverCtrl: PopoverController) { }
+  constructor() { }
 
-  presentPopover(event: Event) {
-    let popover = this.popoverCtrl.create(PopoverPage);
-    popover.present({ ev: event });
-  }
+
 }
